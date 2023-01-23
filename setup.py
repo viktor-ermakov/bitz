475d4371ebef292ae0899ffcc67d4159375282ac
victor.ermakov@gmail.com <victor.ermakov@gmail.com>
2023-01-23 17:34:11 +0000

    Initial version
    Defined basic structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='bitz-ds',
+   version='0.0.1',
+   description='Classes and functions defined for Data Science at Bitz',
+   author='Viktor Ermakov',
+   author_email='victor.ermakov@gmail.com',
+   packages=['bitz-ds'],  #same as name
+   install_requires=[], #external packages as dependencies
+)

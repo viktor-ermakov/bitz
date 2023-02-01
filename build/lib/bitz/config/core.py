a19844032e5ec0d5300f7358acebbe09a9ad6856
victor.ermakov@gmail.com <victor.ermakov@gmail.com>
2023-02-01 21:49:57 +0000

    Added strictyaml requirements

--- build/lib/bitz/config/core.py
+++ build/lib/bitz/config/core.py
@@ -2,8 +2,9 @@ from pathlib import Path
 from typing import Dict, List, Sequence
 
 from pydantic import BaseModel
+from strictyaml import YAML, load
 
-import regression_model
+import bitz
 
 # Project Directories
 PACKAGE_ROOT = Path(bitz.__file__).resolve().parent

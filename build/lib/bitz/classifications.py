ae04a0a8a77c7d2baefb1f6f3f354e3a157e50d3
victor.ermakov@gmail.com <victor.ermakov@gmail.com>
2023-02-06 18:26:54 +0000

    kwarg added to RFVClassificator

--- build/lib/bitz/classifications.py
+++ build/lib/bitz/classifications.py
@@ -24,7 +24,7 @@ class RFVClassificator:
         
     def fit(self, X, y = None):
         
-        self.transactions = TransactionsValidator(X.copy())
+        self.transactions = TransactionsValidator(transactions=X.copy())
         self.transactions['value'] = self.transactions['value'].abs()
         
         stacked_query = 'select cast(idConta as int) as aid from trusted.mysql_contas where idConta NOT IN (4,5,7,8,9,67,179,183) and status_conta = "Não Apta"'
@@ -146,4 +146,4 @@ class RFVClassificator:
             "rfv.rfv_group_summary_" + str(date.today()).replace("-", "_")
         )
     
-        print("RFV results saved sucessfully!")
+        print("RFV results saved sucessfully!")transactions=
